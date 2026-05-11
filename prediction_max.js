e7cd2721582022fb2c8c1b5978fc2cdf242f1b7b
AUlyRikas <131946392+AUlyRikas@users.noreply.github.com>
2026-05-12 00:06:51 +0800

    Add files via upload

---
+++ prediction_max.js
@@ -0,0 +1,35 @@
+var predictionMaxData = {
+  "time": "2026-05-11 21:32:32",
+  "issue": "2026131",
+  "code": "12,40,21,32,1,25,7",
+  "zodiac": "羊,兔,狗,猪,马,马,鼠",
+  "wave": "red,red,green,green,red,blue,red",
+  "teSx": "鼠",
+  "teWei": 7,
+  "nextIssue": "2026132",
+  "kills": [
+    "猪",
+    "马",
+    "虎",
+    "龙",
+    "狗"
+  ],
+  "ninePool": [
+    "羊",
+    "猴",
+    "鸡",
+    "鼠",
+    "牛",
+    "兔",
+    "蛇"
+  ],
+  "sixPool": [
+    "羊",
+    "鼠",
+    "蛇",
+    "鸡",
+    "牛",
+    "兔"
+  ],
+  "filled": []
+};
